2d786caa9f139ff4f687d5a4057cfd7dd1d5232c
mowimisha <michaelmagero2@gmail.com>
2021-07-05 16:58:40 +0300

    added about who and our story sections

--- resources/views/frontend/about.blade.php
+++ resources/views/frontend/about.blade.php
@@ -14,6 +14,24 @@
         <div class="container">
             <div class="row mt-5 mb-5">
                 <div class="col-md-6">
+                    <h4 class="text-black title-backgrounds pt-2 pb-2 pl-3 mb-3 ">Who Are We</h4>
+                    <p>We are building Kenya's and Africa's first paperless car-financing platform that empowers our customers to conveniently buy and own cars
+                        with simple and affordable monthly plans.
+                    </p>
+                </div>
+            </div>
+
+
+            <div class="row mt-5 mb-5">
+                <div class="col-md-6">
+                    <h4 class="text-black title-backgrounds pt-2 pb-2 pl-3 mb-3 ">Our Story</h4>
+                    <p>Established to help aspiring car owners have a seamless and hussle free experience to shop for cars, consent to agreements and have their cars collected or delivered, all online and on one platform.
+                    </p>
+                </div>
+            </div>
+
+            <div class="row mt-5 mb-5">
+                <div class="col-md-6">
                     <h4 class="text-black title-backgrounds pt-2 pb-2 pl-3 mb-3 ">Our Mission</h4>
                     <p>Our mission is to treat both customers and employees like guests in our home and make certain our home is a place people want to be. We ensure that every transaction is guided by the principals of honesty, integrity and loyalty.</p>
                 </div>
